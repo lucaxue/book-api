e82fba11770dc0486827bcd56683769e9e3a40ed
lucaxue <lxue02@gmail.com>
2021-03-03 12:10:06 +0000

    testing

--- BookApi/Models/Book.cs
+++ BookApi/Models/Book.cs
@@ -3,12 +3,16 @@ public class Book
 {
     public long? Id { get; set; }
 
+    //model validation
     [Required]
-    [StringLength(2)]
+    [StringLength(20)]
     public string Title { get; set; }
 
     [Required]
-    [StringLength(2)]
+    [StringLength(10)]
+    //model binding - datatype
+    [DataType(DataType.Text)]
+
     public string Author { get; set; }
 }
 

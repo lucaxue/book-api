85befd3b7738a9a7fdfd5e8dd99362a15a6f9126
lucaxue <lxue02@gmail.com>
2021-03-03 11:58:48 +0000

    restrictions

--- BookApi/Models/Book.cs
+++ BookApi/Models/Book.cs
@@ -1,8 +1,14 @@
-
+using System.ComponentModel.DataAnnotations;
 public class Book
 {
     public long? Id { get; set; }
+
+    [Required]
+    [StringLength(2)]
     public string Title { get; set; }
+
+    [Required]
+    [StringLength(2)]
     public string Author { get; set; }
 }
 

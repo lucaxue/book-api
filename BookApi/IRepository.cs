a40d06e0c3b36df3aa77374e83fd334f12a2696a
lucaxue <lxue02@gmail.com>
2021-03-04 19:56:26 +0000

    refactored controller & repo + tried moq testing

--- BookApi/IRepository.cs
+++ BookApi/IRepository.cs
@@ -2,13 +2,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 public interface IRepository<T>
 {
-    Task<IEnumerable<T>> GetAll();
-    void Delete(long id);
-    Task<T> Get(long id);
-    Task<T> Update(T t);
-    Task<T> Insert(T t);
+  Task<IEnumerable<T>> GetAll();
+  void Delete(long id);
+  Task<T> Get(long id);
+  Task<T> Update(T t);
+  Task<T> Insert(T t);
 
-    Task<IEnumerable<T>> Search(string s);
-    Task<IEnumerable<T>> Limit(int l, int p);
-    Task<IEnumerable<T>> SearchAndLimit(string s, int l, int p);
+  Task<IEnumerable<T>> Search(string s, int l, int p);
+
 }
--- BookApi/Models/Book.cs
+++ BookApi/Models/Book.cs
@@ -1,18 +1,18 @@
 using System.ComponentModel.DataAnnotations;
 public class Book
 {
-    public long? Id { get; set; }
+  public long? Id { get; set; }
 
-    //model validation
-    [Required]
-    [StringLength(50)]
-    public string Title { get; set; }
+  //model validation
+  [Required]
+  [StringLength(50)]
+  public string Title { get; set; }
 
-    [Required]
-    [StringLength(30)]
-    //model binding - datatype
-    [DataType(DataType.Text)]
+  [Required]
+  [StringLength(30)]
+  //model binding - datatype
+  [DataType(DataType.Text)]
 
-    public string Author { get; set; }
+  public string Author { get; set; }
 }
 

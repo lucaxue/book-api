cf76f464e28d1284714312531abab84eda24a8e6
lucaxue <lxue02@gmail.com>
2021-06-27 16:36:26 +0000

    Add Namespaces

--- src/Controllers/BookController.cs
+++ src/Controllers/BookController.cs
@@ -1,94 +1,98 @@
-﻿using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BookApi.Models;
+using BookApi.Data;
 
-
-[ApiController]
-[Route("[controller]s")]
-public class BookController : ControllerBase
+namespace BookApi.Controllers
 {
-    private readonly IRepository<Book> _bookRepository;
-
-    public BookController(IRepository<Book> bookRepository)
+    [ApiController]
+    [Route("[controller]s")]
+    public class BookController : ControllerBase
     {
-        _bookRepository = bookRepository;
-    }
+        private readonly IRepository<Book> _bookRepository;
 
-    [HttpGet]
-    public async Task<IActionResult> Index(string search = "", int limit = 100, int page = 1)
-    {
-        try
+        public BookController(IRepository<Book> bookRepository)
         {
-            var booksResult = await _bookRepository.Search(search, limit, page);
-            return Ok(booksResult);
+            _bookRepository = bookRepository;
         }
-        catch (Exception)
+
+        [HttpGet]
+        public async Task<IActionResult> Index(string search = "", int limit = 100, int page = 1)
         {
-            if (limit < 0 || page <= 0)
+            try
             {
-                return BadRequest($"Sorry, the {(page <= 0 ? "page" : "limit")} entered is not valid.\nTry entering a positive number.");
+                var booksResult = await _bookRepository.Search(search, limit, page);
+                return Ok(booksResult);
             }
-            return NotFound("Sorry, could not get any books from the repository.\nPlease try another request.");
+            catch (Exception)
+            {
+                if (limit < 0 || page <= 0)
+                {
+                    return BadRequest($"Sorry, the {(page <= 0 ? "page" : "limit")} entered is not valid.\nTry entering a positive number.");
+                }
+                return NotFound("Sorry, could not get any books from the repository.\nPlease try another request.");
+            }
         }
-    }
 
 
-    [HttpGet("{id}")]
-    public async Task<IActionResult> Show(long id)
-    {
-        try
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Show(long id)
         {
-            var returnedBook = await _bookRepository.Find(id);
-            return Ok(returnedBook);
+            try
+            {
+                var returnedBook = await _bookRepository.Find(id);
+                return Ok(returnedBook);
+            }
+            catch (Exception)
+            {
+                return NotFound($"Sorry, book of id {id} cannot be fetched, since it does not exist.\nAre you sure the id is correct?");
+            }
         }
-        catch (Exception)
-        {
-            return NotFound($"Sorry, book of id {id} cannot be fetched, since it does not exist.\nAre you sure the id is correct?");
-        }
-    }
 
-    [HttpPost]
-    public async Task<IActionResult> Store([FromBody] Book book)
-    {
-        try
+        [HttpPost]
+        public async Task<IActionResult> Store([FromBody] Book book)
         {
-            var insertedBook = await _bookRepository.Create(book);
-            return Created($"/books/{insertedBook.Id}", insertedBook);
+            try
+            {
+                var insertedBook = await _bookRepository.Create(book);
+                return Created($"/books/{insertedBook.Id}", insertedBook);
+            }
+            catch (Exception)
+            {
+                return BadRequest($"Sorry, cannot insert new book.\nAre you sure the book is valid?");
+            }
         }
-        catch (Exception)
-        {
-            return BadRequest($"Sorry, cannot insert new book.\nAre you sure the book is valid?");
-        }
-    }
 
-    [HttpPut("{id}")]
-    public async Task<IActionResult> Update(long id, [FromBody] Book book)
-    {
-        try
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(long id, [FromBody] Book book)
         {
-            book.Id = id;
-            var updatedBook = await _bookRepository.Update(book);
-            return Ok(updatedBook);
+            try
+            {
+                book.Id = id;
+                var updatedBook = await _bookRepository.Update(book);
+                return Ok(updatedBook);
+            }
+            catch (Exception)
+            {
+                return BadRequest($"Sorry, book of id {id} cannot be updated, since it does not exist.\nAre you sure the id is correct?");
+            }
         }
-        catch (Exception)
-        {
-            return BadRequest($"Sorry, book of id {id} cannot be updated, since it does not exist.\nAre you sure the id is correct?");
-        }
-    }
 
 
-    [HttpDelete("{id}")]
-    public IActionResult Destroy(long id)
-    {
-        try
+        [HttpDelete("{id}")]
+        public IActionResult Destroy(long id)
         {
-            _bookRepository.Delete(id);
-            return Ok();
-        }
-        catch (Exception)
-        {
-            return BadRequest($"Sorry, book of id {id} cannot be deleted, since it does not exit.\nAre you sure the id is correct?");
+            try
+            {
+                _bookRepository.Delete(id);
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest($"Sorry, book of id {id} cannot be deleted, since it does not exit.\nAre you sure the id is correct?");
+            }
         }
     }
 }

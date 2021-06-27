79038b62fcd89d44052f59131c62079a57028cd6
lucaxue <lxue02@gmail.com>
2021-06-27 15:51:22 +0000

    Refactor to use Convenction Method Names

--- src/Data/IRepository.cs
+++ src/Data/IRepository.cs
@@ -2,12 +2,9 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 public interface IRepository<T>
 {
-  Task<IEnumerable<T>> GetAll();
-  void Delete(long id);
-  Task<T> Get(long id);
-  Task<T> Update(T t);
-  Task<T> Insert(T t);
-
   Task<IEnumerable<T>> Search(string s, int l, int p);
-
+  Task<T> Find(long id);
+  Task<T> Create(T t);
+  Task<T> Update(T t);
+  void Delete(long id);
 }

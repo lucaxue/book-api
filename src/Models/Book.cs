373da3ea73e706c75fe72e400576f762150793a0
lucaxue <lxue02@gmail.com>
2021-06-27 16:28:00 +0000

    Indentation

--- src/Models/Book.cs
+++ src/Models/Book.cs
@@ -1,18 +1,17 @@
 using System.ComponentModel.DataAnnotations;
 public class Book
 {
-  public long? Id { get; set; }
+    public long? Id { get; set; }
 
-  //model validation
-  [Required]
-  [StringLength(50)]
-  public string Title { get; set; }
+    [Required]
+    [StringLength(50)]
+    [DataType(DataType.Text)]
+    public string Title { get; set; }
 
-  [Required]
-  [StringLength(30)]
-  //model binding - datatype
-  [DataType(DataType.Text)]
+    [Required]
+    [StringLength(30)]
+    [DataType(DataType.Text)]
 
-  public string Author { get; set; }
+    public string Author { get; set; }
 }
 
